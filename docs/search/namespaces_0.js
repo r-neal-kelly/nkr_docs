d08b06dd6e0eb1bd5f506e11d6572e3d6176108c
r-neal-kelly <r.neal.kelly@gmail.com>
2021-10-02 17:33:03 -0400

    rebuilt the docs after the latest updates on nkr

--- docs/search/namespaces_0.js
+++ docs/search/namespaces_0.js
@@ -4,9 +4,10 @@ var searchData=
   ['_24built_5fin_5fsp_1',['$built_in_sp',['../namespacenkr_1_1_0bmaybe__t_1_1_0bbuilt__in__sp.html',1,'nkr::$maybe_t']]],
   ['_24maybe_5ft_2',['$maybe_t',['../namespacenkr_1_1_0bmaybe__t.html',1,'nkr']]],
   ['_24pointer_5ft_3',['$pointer_t',['../namespacenkr_1_1_0bpointer__t.html',1,'nkr']]],
-  ['atomic_4',['atomic',['../namespacenkr_1_1os_1_1atomic.html',1,'nkr::os']]],
-  ['endian_5',['endian',['../namespacenkr_1_1os_1_1endian.html',1,'nkr::os']]],
-  ['heap_6',['heap',['../namespacenkr_1_1os_1_1heap.html',1,'nkr::os']]],
-  ['nkr_7',['nkr',['../namespacenkr.html',1,'']]],
-  ['os_8',['os',['../namespacenkr_1_1os.html',1,'nkr']]]
+  ['allocator_4',['allocator',['../namespacenkr_1_1allocator.html',1,'nkr']]],
+  ['atomic_5',['atomic',['../namespacenkr_1_1os_1_1atomic.html',1,'nkr::os']]],
+  ['endian_6',['endian',['../namespacenkr_1_1os_1_1endian.html',1,'nkr::os']]],
+  ['heap_7',['heap',['../namespacenkr_1_1os_1_1heap.html',1,'nkr::os']]],
+  ['nkr_8',['nkr',['../namespacenkr.html',1,'']]],
+  ['os_9',['os',['../namespacenkr_1_1os.html',1,'nkr']]]
 ];
