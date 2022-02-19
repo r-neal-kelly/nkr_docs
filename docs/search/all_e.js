dcd89453fdc1f4eb9aa5d9c908a721757f4abd14
r-neal-kelly <r.neal.kelly@gmail.com>
2022-02-19 15:50:47 -0500

    added more docs for tr

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -1,8 +1,10 @@
 var searchData=
 [
-  ['tools_0',['Tools',['../md_include_markdown_tools.html',1,'']]],
-  ['tr_1',['TR',['../namespacenkr.html#a1898ff80c7081762a1840f00883fa258',1,'nkr']]],
-  ['traits_2',['traits',['../group____a5f738af__46d1__4576__aaf6__adbc60dc07fe.html',1,'']]],
-  ['ts_3',['ts',['../namespacenkr.html#ae4babe1476e170123b9682b67160a0e8',1,'nkr']]],
-  ['tts_4',['tts',['../namespacenkr.html#aa41183e8bf88f05fd2c80a26fa6ebb52',1,'nkr']]]
+  ['t_0',['t',['../namespacenkr.html#ac2fadabeca6af1088b2114f4b32cc63f',1,'nkr']]],
+  ['tools_1',['Tools',['../md_include_markdown_tools.html',1,'']]],
+  ['tr_2',['TR',['../namespacenkr.html#a1898ff80c7081762a1840f00883fa258',1,'nkr']]],
+  ['traits_3',['traits',['../group____a5f738af__46d1__4576__aaf6__adbc60dc07fe.html',1,'']]],
+  ['ts_4',['ts',['../namespacenkr.html#ae4babe1476e170123b9682b67160a0e8',1,'nkr']]],
+  ['tt_5',['tt',['../namespacenkr.html#ad9ca1dc69cb9a052d8bc183cf5e69dcb',1,'nkr']]],
+  ['tts_6',['tts',['../namespacenkr.html#aa41183e8bf88f05fd2c80a26fa6ebb52',1,'nkr']]]
 ];
--- docs/search/typedefs_7.js
+++ docs/search/typedefs_7.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['ts_0',['ts',['../namespacenkr.html#ae4babe1476e170123b9682b67160a0e8',1,'nkr']]],
-  ['tts_1',['tts',['../namespacenkr.html#aa41183e8bf88f05fd2c80a26fa6ebb52',1,'nkr']]]
+  ['t_0',['t',['../namespacenkr.html#ac2fadabeca6af1088b2114f4b32cc63f',1,'nkr']]],
+  ['ts_1',['ts',['../namespacenkr.html#ae4babe1476e170123b9682b67160a0e8',1,'nkr']]],
+  ['tt_2',['tt',['../namespacenkr.html#ad9ca1dc69cb9a052d8bc183cf5e69dcb',1,'nkr']]],
+  ['tts_3',['tts',['../namespacenkr.html#aa41183e8bf88f05fd2c80a26fa6ebb52',1,'nkr']]]
 ];
